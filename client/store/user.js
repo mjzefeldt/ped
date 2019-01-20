d9646430fffd343b426c070ec372006d4bb4cb61
MJ Zefeldt <mjzefeldt@gmail.com>
2019-01-20 00:00:58 -0600

    deleting from store/user.js

--- client/store/user.js
+++ client/store/user.js
@@ -24,29 +24,6 @@ const removeUser = () => ({type: REMOVE_USER})
  * THUNK CREATORS
  */
 
-// fetch now installed if want to try that
-// export const fetchProfile = (url) => async dispatch => {
-//   console.log('thunk before call')
-
-//   try {
-//     const res = await axios.post('https://api.fitbit.com/oauth2/token', {
-//       method: 'POST',
-//       headers: {
-//         'Authorization': 'Basic MjJEQjlROjEzNWM2YzI3NjIxZTY1NWRlZWVlYTQwNDNhMjA2MzU2',
-//         'Content-Type': 'application/x-www-form-urlencoded'
-//       },
-//       body: `client_id=22DB9Q&grant_type=authorization_code&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fauth%2Ffitbit%2Fcallback&code=${url}`
-//     })
-//     console.log(res.data, '<<< res in thunk')
-//   } catch (err) {
-//     console.error(err)
-//   }
-// }
-// client_id=22DB9Q&grant_type=authorization_code&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fauth%2Ffitbit%2Fcallback&code=303739859f45e8230dec0fff048949410a8900c0
-
-// curl -X POST -i -H 'Authorization: Basic MjJEQjlROjEzNWM2YzI3NjIxZTY1NWRlZWVlYTQwNDNhMjA2MzU2' -H 'Content-Type: application/x-www-form-urlencoded'
-// -d "clientId=22DB9Q" -d "grant_type=authorization_code" -d "redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fauth%2Ffitbit%2Fcallback" -d "code=7b2dfdab0a80d3dad6c5c93c30de03e2e0224a5c" https://api.fitbit.com/oauth2/token
-
 export const me = () => async dispatch => {
   try {
     const res = await axios.get('/auth/me')
